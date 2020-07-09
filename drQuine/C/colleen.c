5a8f4f81aeb5e5e3e0ddc1a364e32cbd982a3a35
aqw42 <corentin.noel56@gmail.com>
2020-07-09 18:15:27 +0200

    colleen

--- drQuine/C/colleen.c
+++ drQuine/C/colleen.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+void func(){
+}
 //q
 int	main(void)
 {
 	//q
-	char *s = "#include <stdio.h>%c%c//q%cint	main(void)%c{%c	//q%c	char *s = %c%s%c;%c	printf(s, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10);%c}";
-	printf(s, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10);
+	char *s = "#include <stdio.h>%c%cvoid func(){%c}%c//q%cint	main(void)%c{%c	//q%c	char *s = %c%s%c;%c	printf(s, 10, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10);%c}";
+	printf(s, 10, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10);
 }
