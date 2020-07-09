d0040d365dec4550ae197a81b6a108a3fe0bc27a
aqw42 <corentin.noel56@gmail.com>
2020-07-09 18:13:10 +0200

    grace

--- drQuine/C/grace.c
+++ drQuine/C/grace.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-//q
-int	main(void)
-{
-	int a = 0;
-	char *s = "#include <stdio.h>%c#include <fcntl.h>%c%c//q%cint	main(void)%c{%c	//q%c	int a = 0;%c	char *s = %c%s%c;%c	a = open(%c./grace_kid.c%c, O_RDWR | O_CREAT | S_IRWXU);%c	dprintf(a, s, 10, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 34, 34, 10, 10);%c}";
-	a = open("./grace_kid.c", O_RDWR | O_CREAT | S_IRWXU);
-	dprintf(a, s, 10, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 34, 34, 10, 10);
-}
+#define LAUNCH main(){int a = 0; char *s = CODE; PRINT}
+#define PRINT  a = open("./grace_kid.c", O_RDWR | O_CREAT | S_IRWXU); dprintf(a, s, 10, 10, 10, 10, 34, 34, 10, 34, s, 34, 10);
+#define CODE "#include <stdio.h>%c#include <fcntl.h>%c%c#define LAUNCH main(){int a = 0; char *s = CODE; PRINT}%c#define PRINT  a = open(%c./grace_kid.c%c, O_RDWR | O_CREAT | S_IRWXU); dprintf(a, s, 10, 10, 10, 10, 34, 34, 10, 34, s, 34, 10);%c#define CODE %c%s%c%cLAUNCH"
+LAUNCH
