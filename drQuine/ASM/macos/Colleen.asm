cc1f23d3cfd59a5b7a46ea4953ae206e967a6f69
corentin <garsvog1@gmail.com>
2020-07-13 01:49:49 +0200

    pause here

--- drQuine/ASM/macos/Colleen.asm
+++ drQuine/ASM/macos/Colleen.asm
@@ -1,17 +1,17 @@
-DEFAULT REL
 SECTION .text
 global _main
 extern _printf
 extern _exit
 _main:
 	push rbx
-	lea rdi, [code]
+	lea rdi, [rel code]
 	mov rsi, 10
 	mov rdx, 34
-	lea rcx, [code]
+	lea rcx, [rel code]
 	xor rax, rax
 	call _printf
 quit:
 	call _exit
 
+SECTION .rodata
 code: db "DEFAULT REL%1$cSECTION .text%1$cglobal _main%1$cextern _printf%1$c_main:%1$c	push rbx%1$c	lea rdi, [code]%1$c	mov rsi, 10%1$c	mov rdx, 34%1$c	lea rcx, [code]%1$c	xor rax, rax%1$c	call _printf%1$cquit:%1$c	mov eax, 0x2000001%1$c	xor edi, edi%1$c	syscall ;end%1$c%1$ccode: %2$c%3$s%2$c, 0%1$c", 0
