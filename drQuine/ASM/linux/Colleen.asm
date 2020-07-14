ba8ac9996e21cc67dc28c9f93890492b7a18e5c0
aqw42 <corentin.noel56@gmail.com>
2020-07-14 03:29:18 +0200

    Colleen / Grace DONE ASM linux ; Sully almost finished ASM linux

--- drQuine/ASM/linux/Colleen.asm
+++ drQuine/ASM/linux/Colleen.asm
@@ -1,6 +1,6 @@
 DEFAULT REL
 SECTION .rodata
-code: db "DEFAULT REL%1$cSECTION .rodata%1$ccode: db %2$c%3$s%2$c%1$c%1$cSECTION .text%1$cextern printf%1$cglobal _start%1$c_start:%1$c	mov edi, code%1$c	mov esi, 10%1$c	mov edx, 34%1$c	mov ecx, code%1$c	xor eax, eax%1$c	call printf%1$c%1$c	mov eax, 60%1$c	xor edi, edi%1$c	syscall ;end%1$c"
+code: db "DEFAULT REL%1$cSECTION .rodata%1$ccode: db %2$c%3$s%2$c, 0%1$c%1$cSECTION .text%1$cextern printf%1$cextern exit%1$cglobal _start%1$c_start:%1$c	mov edi, code%1$c	mov esi, 10%1$c	mov edx, 34%1$c	mov ecx, code%1$c	xor eax, eax%1$c	call printf%1$cend:%1$c	call exit ;end%1$c", 0
 
 SECTION .text
 extern printf
@@ -13,5 +13,5 @@ _start:
 	mov ecx, code
 	xor eax, eax
 	call printf
-
-	call exit
+end:
+	call exit ;end
